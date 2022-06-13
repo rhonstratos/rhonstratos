a285e6f971ec136c29191208846a2b40403d32ab
rhonstratos <kizunaai20@gmail.com>
2022-06-13 20:52:40 +0800

    tailwind css

---
+++ src/js/App.jsx
@@ -0,0 +1,10 @@
+import "../css/tailwind.build.css"
+const App = ()=> {
+  return (
+    <div className="App text-center">
+      <h1 className="text-center bg-{#454545}">test</h1>
+    </div>
+  );
+}
+
+export default App;
